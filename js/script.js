cfdf288b908bd6e6ed846737f665deee245f0cf5
giomen88 <giorgia9288@virgilio.it>
2022-06-22 23:08:39 +0200

    finito senza validazione

--- js/script.js
+++ js/script.js
@@ -1,21 +1,11 @@
-// - CHIEDERE CHILOMETRI DA PERCORRERE
-// - CHIEDERE ETA'
-// - CALCOLARE PREZZO DEL BIGLIETTO (0.21€ PER KM)
-// - SCONTO 20% MINORENNI
-// - SCONTO 40% OVER 65
-// - PREZZO STAMPATO IN PAGINA CON MASSIMO DUE DECIMALI (CENTESIMI)
 
 const firstName = document.getElementById('first-name')
-console.log('nome ' + firstName.value)
 
 const lastName = document.getElementById('last-name')
-console.log('cognome ' + lastName.value)
 
 const distance = document.getElementById('distance')
-console.log('km ' + distance.value)
 
 const age = document.getElementById('age')
-console.log('età ' + age.value)
 
 const create = document.getElementById('create')
 
@@ -30,43 +20,70 @@ const surnameParagraph = document.getElementById('surname-paragraph')
 
 const seatParagraph = document.getElementById('seat-paragraph')
 
+const codeParagraph = document.getElementById('code-paragraph')
+
+const rateParagraph = document.getElementById('rate-paragraph')
+
 const discountParagraph = document.getElementById('discount-paragraph')
 
+// VALIDAZIONE
 
 
+// if (isNaN(distance.value) || distance.value < 1 || distance.value == '') {
+//     alert('La distanza inserita non è corretta!')
+// }
+
+// else {
 
 create.addEventListener('click', function () {
 
     let price = parseInt(distance.value * 0.21);
-    console.log(price)
+    console.log(price);
 
-    const random = Math.random()
+    const random = Math.random();
 
-    const minSeat = 1
-    const maxSeat = 100
+    const minSeat = 1;
+    const maxSeat = 100;
     const seat = Math.floor(random * (maxSeat + 1)) + minSeat;
+
+    const minCode = 4000;
+    const maxCode = 9000;
+    const code = Math.floor(random * (maxCode + 1)) + minCode;
+
+    const discountMessage = 'Hai uno sconto del ';
+
+    let rate = 'Adult';
 
     if (age.value === 'minor') {
         price = (price - price * 20 / 100).toFixed(2)
+        rate = 'Junior'
+        discountParagraph.innerHTML = discountMessage + '<strong>20%</strong>'
     }
 
     else if (age.value === 'senior') {
         price = (price - price * 40 / 100).toFixed(2)
+        rate = 'Senior'
+        discountParagraph.innerHTML = discountMessage + '<strong>40%</strong>'
     }
 
-    nameParagraph.innerHTML = firstName.value;
-    surnameParagraph.innerHTML = lastName.value;
-    priceParagraph.innerHTML = '€ ' + price;
-    seatParagraph.innerHTML = seat;
+    nameParagraph.innerHTML = `<strong>${firstName.value}</strong>`;
+    surnameParagraph.innerHTML = `<strong>${lastName.value}</strong>`;
+    seatParagraph.innerHTML = `<strong>${seat}</strong>`;
+    codeParagraph.innerHTML = `<strong>${code}</strong>`;
+    rateParagraph.innerHTML = `<strong>${rate}</strong>`;
+    priceParagraph.innerHTML = `<strong>€${price}</strong>`;
 })
 
 cancel.addEventListener('click', function () {
 
-    firstName.value = ''
-    lastName.value = ''
-    distance.value = ''
+    firstName.value = '';
+    lastName.value = '';
+    distance.value = '';
 
     nameParagraph.innerHTML = '';
     surnameParagraph.innerHTML = '';
+    seatParagraph.innerHTML = '';
+    rateParagraph.innerHTML = '';
     priceParagraph.innerHTML = '';
 })
+// }
